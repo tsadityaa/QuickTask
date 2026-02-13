caab3f3df3487fc68fd46e30e4b94eac90863f36
Aditya <t.s.aditya35@gmail.com>
2026-02-14 00:12:43 +0530

    Enable CORS for all origins on backend

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
